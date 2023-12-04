b69c31791d317256281d2cfdea1c03bb9a05ca8c
rouakhadhar4 <147189044+rouakhadhar4@users.noreply.github.com>
2023-12-04 15:56:33 +0100

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,28 @@
-
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<h1>{{title}}</h1><p>Length: {{title.length}}</p><p>Reversed: {{getReversed(title)}}</p>',
+  template: `
+    <input #input type="text" (input)="textInput($event)" value=""/>
+    <hr>
+    Upper-Case: {{ upperCase }}
+    <br/>
+    Lower-Case: {{ lowerCase }}
+  `,
   styles: []
 })
+export class AppComponent implements AfterViewInit {
+  upperCase: string = '';
+  lowerCase: string = '';
+  
+  @ViewChild('input') inputBox;
 
-export class AppComponent {
-  title = 'app works!';
+  textInput(event) {
+    this.upperCase = event.target.value.toUpperCase();
+    this.lowerCase = event.target.value.toLowerCase();
+  }
 
-  getReversed(str: string) {
-    let reversed = '';
-    for (let i = str.length - 1; i >= 0; i--) {
-      reversed += str.substring(i, i + 1);
-    }
-    return reversed;
+  ngAfterViewInit() {
+    this.inputBox.nativeElement.focus();
   }
 }
