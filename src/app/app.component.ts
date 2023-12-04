b0635012bbc8608631ba6217fcde7cf5246a0a54
rouakhadhar4 <147189044+rouakhadhar4@users.noreply.github.com>
2023-12-04 15:54:27 +0100

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,20 @@
-import { Component } from "@angular/core";
-import { Car } from './car';
+
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <table>
-      <tr *ngFor="let car of cars">
-        <td>{{ car.make }}</td>
-        <td>{{ car.model }}</td>
-        <td><button (click)="showCar(car)">Click me!</button></td> <!-- Appel de showCar avec l'argument car -->
-      </tr>
-    </table>
-  `,
+  template: '<h1>{{title}}</h1><p>Length: {{title.length}}</p><p>Reversed: {{getReversed(title)}}</p>',
   styles: []
 })
-export class AppComponent {
-  cars = [
-    new Car('car1', 2002, 'bmw', 'm3', 'https://en.wikipedia.org/wiki/BMW_M3'),
-    new Car('car2', 2017, 'acura', 'nsx', 'https://en.wikipedia.org/wiki/Honda_NSX'),
-    new Car('car3', 2016, 'chevy', 'camaro', 'https://en.wikipedia.org/wiki/Chevrolet_Camaro')
-  ];
 
-  showCar(car: Car) { // Utilisation du type "Car" pour l'argument car
-    const description = car.make;
-    if (window.confirm('Click "ok" to be redirected to ' + car.article)) {
-      window.location.href = car.article; // Correction de la propriété "href"
+export class AppComponent {
+  title = 'app works!';
+
+  getReversed(str: string) {
+    let reversed = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+      reversed += str.substring(i, i + 1);
     }
+    return reversed;
   }
 }
