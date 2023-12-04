8b425c5551eabdfdb2e5ee0de065de8f5d9d3f64
rouakhadhar4 <147189044+rouakhadhar4@users.noreply.github.com>
2023-12-04 15:52:41 +0100

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,30 @@
 import { Component } from "@angular/core";
+import { Car } from './car';
 
 @Component({
   selector: 'app-root',
   template: `
-    <p>Foreground: <input [(ngModel)]="fg" /></p>
-    <p>Background: <input [(ngModel)]="bg" /></p>
-
-    <div [ngStyle]="{'color': fg, 'background-color': bg, 'padding': '5px'}">
-      Zone de Teste
-    </div>
+    <table>
+      <tr *ngFor="let car of cars">
+        <td>{{ car.make }}</td>
+        <td>{{ car.model }}</td>
+        <td><button (click)="showCar(car)">Click me!</button></td> <!-- Appel de showCar avec l'argument car -->
+      </tr>
+    </table>
   `,
   styles: []
 })
 export class AppComponent {
-  fg = "#ffffff";
-  bg = "#000000";
+  cars = [
+    new Car('car1', 2002, 'bmw', 'm3', 'https://en.wikipedia.org/wiki/BMW_M3'),
+    new Car('car2', 2017, 'acura', 'nsx', 'https://en.wikipedia.org/wiki/Honda_NSX'),
+    new Car('car3', 2016, 'chevy', 'camaro', 'https://en.wikipedia.org/wiki/Chevrolet_Camaro')
+  ];
+
+  showCar(car: Car) { // Utilisation du type "Car" pour l'argument car
+    const description = car.make;
+    if (window.confirm('Click "ok" to be redirected to ' + car.article)) {
+      window.location.href = car.article; // Correction de la propriété "href"
+    }
+  }
 }
-
-
-
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import {BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
+
 @NgModule({
-declarations: [
-AppComponent
-],
-imports: [
-BrowserModule,
-FormsModule
-],
-providers: [],
-bootstrap: [AppComponent]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
